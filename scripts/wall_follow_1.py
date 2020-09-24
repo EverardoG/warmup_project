bd6951f27af40138887c909a3b02f4345c23d400
EverardoG <evergon907@gmail.com>
2020-09-24 00:30:26 -0400

    wall follow with line approx

--- scripts/wall_follow_1.py
+++ scripts/wall_follow_1.py
@@ -36,14 +36,15 @@ class WallFollowNode(object):
         self.max_angular_speed = 1.0
 
         # Make data containers easily accessible for later
-        self.points_r_theta = np.zeros((2, self.scan_range))
-        self.points_x_y = np.zeros((2, self.scan_range))
+        self.points_r_theta = None # np.zeros((2, self.scan_range))
+        self.points_x_y = None # np.zeros((2, self.scan_range))
         self.desired_angle = None # wall angle
 
     def keyWasPressed(self):
         return select.select([sys.stdin], [], [], 0) == ([sys.stdin], [], [])
 
     def callbackFunc(self, msg):
+        # print("Running callback \r")
         # # update both points
         # self.point_1 = msg.ranges[315]
         # self.point_2 = msg.ranges[225]
@@ -51,35 +52,74 @@ class WallFollowNode(object):
         # update point data
         start_angle = int(self.scan_angle-self.scan_range/2)
         end_angle = int(self.scan_angle+self.scan_range/2)
+        # print(msg.ranges[start_angle:end_angle],'\r')
+        self.points_r_theta = []
+        self.points_x_y = []
         # print(start_angle)
         for ind, range_ in enumerate(msg.ranges[start_angle:end_angle]):
+            # print("loop\r")
             current_angle = ind + start_angle
             # self.points_r_theta[ind, 0] = range_
             # self.points_r_theta[ind, 1] = current_angle
 
             if np.isfinite(range_):
-            
-                self.points_r_theta[0, ind] = range_
-                self.points_r_theta[1, ind] = current_angle
+                # print('finite\r')
+                self.points_r_theta.append((range_, current_angle))
 
                 x = range_ * np.cos((current_angle)*3.14/180.0)
                 y = range_ * np.sin((current_angle)*3.14/180.0)
-                # self.points_x_y[ind, 0] = x
-                # self.points_x_y[ind, 1] = y
 
-                self.points_x_y[0, ind] = x
-                self.points_x_y[1, ind] = y
+                self.points_x_y.append((x, y))
+        # print(len(self.points_x_y),'\r')
+
+                # self.points_r_theta[0, ind] = range_
+                # self.points_r_theta[1, ind] = current_angle
+
+                # x = range_ * np.cos((current_angle)*3.14/180.0)
+                # y = range_ * np.sin((current_angle)*3.14/180.0)
+                # # self.points_x_y[ind, 0] = x
+                # # self.points_x_y[ind, 1] = y
+
+                # self.points_x_y[0, ind] = x
+                # self.points_x_y[1, ind] = y
+            # print(self.points_x_y,'\r')
 
     def updateDesiredAngle(self):
         # get a best fit line through the points
-        xs = self.points_x_y[:, 0]
-        ys = self.points_x_y[:,1]
+        # returns True if angle updated successfully
+
+        # Check that we have enough info to continue
+        if not self.points_x_y or len(self.points_x_y) < 2:
+            return False
+
+        # print(self.points_x_y,'\r')
+        num_points = len(self.points_x_y)
+
+        # Convert xy list to numpy array
+        # print(type(num_points),'\r')
+        xs = np.zeros(num_points)
+        ys = np.zeros(num_points)
+        for index, point in enumerate(self.points_x_y):
+            xs[index] = point[0]
+            ys[index] = point[1]
+
+        # print("xs", xs,'\r')
+        # print("ys", ys, '\r')
+
+        # xs = self.points_x_y[:, 0]
+        # ys = self.points_x_y[:,1]
         xmin, xmax = min(xs), max(xs)
         pfit, stats = Polynomial.fit(xs, ys, 1, full=True, window=(xmin, xmax), domain=(xmin, xmax))
         _, slope = pfit
+        print("slope: ", slope, '\r')
+        print("tan slope: ", np.arctan(slope), '\r')
+        print("desired angle: ", np.arctan(slope), '\r')
 
         # Turn that into an angle
-        self.desired_angle = np.tan(slope) * 180.0/3.14
+        self.desired_angle = np.arctan(slope) * 180.0/3.14
+        # print(self.desired_angle,'\r')
+
+        return True
 
     def run(self):
         r = rospy.Rate(10)
@@ -107,27 +147,29 @@ class WallFollowNode(object):
             if self.start and self.active:
                 twist_msg = Twist()
 
-                # Update the desired angle
-                self.updateDesiredAngle()
-
                 # Constant linear speed
                 twist_msg.linear.x = 0.3
 
-                # Run Proportional Control.
-                # desired_angle = 0  or 360 if robot is properly aligned
-                # Handle 0 to 180 case separately from 180 to 360 case
-                if (self.desired_angle > 0.0 and self.desired_angle <= 180.0):
-                    twist_msg.angular.z = 0.1 * self.desired_angle
-                else:
-                    twist_msg.angular.z = 0.1 * (360.0 - self.desired_angle)
-                
-                # Enforce limits on angular velocity
-                if twist_msg.angular.z > self.max_angular_speed:
-                    twist_msg.angular.z = self.max_angular_speed
+                # Update the desired angle
+                if self.updateDesiredAngle():
+                    # print(len(self.points_x_y),'\r')
+                    # Run Proportional Control.
+                    # desired_angle = 0  or 360 if robot is properly aligned
+                    print(self.desired_angle,'\r')
+                    twist_msg.angular.z = 0.015 * self.desired_angle
+                    # if (self.desired_angle > 0.0 and self.desired_angle <= 180.0):
+                    #     twist_msg.angular.z = 0.01 * self.desired_angle
+                    # else:
+                    #     twist_msg.angular.z = 0.01 * self.desired_angle
+                    
+                    print("twist_msg.angular.z",twist_msg.angular.z,'\r')
+                    # Enforce limits on angular velocity
+                    if twist_msg.angular.z > self.max_angular_speed:
+                        twist_msg.angular.z = self.max_angular_speed
 
-                elif twist_msg.angular.z < - self.max_angular_speed:
-                    twist_msg.angular.z = - self.max_angular_speed
-                
+                    elif twist_msg.angular.z < - self.max_angular_speed:
+                        twist_msg.angular.z = - self.max_angular_speed
+
 
                 # # Check the difference between the two points
                 # # turn either left or right proportionally dependent on the difference
